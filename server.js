81ea1632db21c04317b33d93d0a55ca9238a1921
arun-kumar-js <srivar63@outlook.com>
2025-01-19 12:04:57 +0530

    third

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require("./app");
 const mongoose = require("mongoose");
 const { MONGODB_URI, PORT } = require("./utils/config");
 const cors = require("cors");
+const morgan = require("morgan");
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
